f566061fb8c022e5e79f50a80e3a814961020e30
devweb550 <devweb550@gmail.com>
2022-11-01 23:17:34 +0300

    store update

--- src/App.svelte
+++ src/App.svelte
@@ -1,163 +1,204 @@
 <script>
   import { Router, Link, Route } from "svelte-navigator";
-  import { Progress, Badge } from 'sveltestrap';
-  
+  import { Progress, Badge } from "sveltestrap";
+
   import Expenses from "./lib/Expenses.svelte";
-  import FeedbackForm from './lib/FeedbackForm.svelte';
-  import FeedbackList from './lib/FeedbackList.svelte'
-  import FeedbackStats from './lib/FeedbackStats.svelte';
+  import FeedbackForm from "./lib/FeedbackForm.svelte";
+  import FeedbackList from "./lib/FeedbackList.svelte";
+  import FeedbackStats from "./lib/FeedbackStats.svelte";
   import Incomes from "./lib/Incomes.svelte";
   import Investments from "./lib/Investments.svelte";
-  import { LanguageStore, FeedbackStore } from "./stores";
+  import {
+    LanguageStore,
+    FeedbackStore,
+    countOfIncome,
+    countOfExpense,
+    countOfInvestment,
+    amountOfExpense,
+    amountOfIncome,
+    amountOfInvestment,
+  } from "./stores";
 
-  
-  
-  let selectedMenu = "transitions"
+  let selectedMenu = "transitions";
 
   const handleLanguage = () => {
-    if($LanguageStore === "EN"){
-      $LanguageStore = "TR"
-    }else{
-      $LanguageStore = "EN"
+    if ($LanguageStore === "EN") {
+      $LanguageStore = "TR";
+    } else {
+      $LanguageStore = "EN";
     }
-  }
+  };
 
   const handleSelectMenu = (e) => {
-    selectedMenu = e.detail
-  }
+    selectedMenu = e.detail;
+  };
 
-  $:count = $FeedbackStore.length
-  $: incomesCount = $FeedbackStore.filter((item) => item.transitionType === "Income").length
-  $: expenseCount = $FeedbackStore.filter((item) => item.transitionType === "Expense").length
-  $: investmentsCount = $FeedbackStore.filter((item) => item.transitionType === "Investment").length
+  $: count = $FeedbackStore.length;
 
   let budget;
-  $: totalExpenseAmount =  $FeedbackStore.filter((transition) => transition.transitionType === "Expense").reduce((a, item) => a + item.amount, 0)
-  $: totalIncomeAmount =  $FeedbackStore.filter((transition) => transition.transitionType === "Income").reduce((a, item) => a + item.amount, 0)
-  $: totalInvestmentAmount =  $FeedbackStore.filter((transition) => transition.transitionType === "Investment").reduce((a, item) => a + item.amount, 0)
- 
-  $: if(count === 0){
-    budget= 0
-  }else{       
-    budget = $FeedbackStore.reduce((a, item) => a + item.amount, 0) - 2*totalExpenseAmount - 2*totalInvestmentAmount
+
+  $: if (count === 0) {
+    budget = 0;
+  } else {
+    budget = $amountOfIncome - $amountOfExpense - $amountOfInvestment;
   }
-
-  
-  
-  
-
 </script>
 
 <div class="box">
-
-
-<Router>
-  <div class="header">
-    <div class="yeni" on:click={() => selectedMenu = "new"} class:link="{selectedMenu === "new"}" >
-      <Link  to="form" >{$LanguageStore === "TR" ? "Yeni İşlem Ekle" : "New Transaction"} <span>+</span> </Link>
-      
-    </div>
-    
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={() => selectedMenu = "transitions"}  class:link="{selectedMenu === "transitions"}" >
-      <Link  to="/" >{$LanguageStore === "TR" ? "Tüm İşlemler" : "All Transactions"}  </Link>
-      
-    </div>
-    
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={() => selectedMenu = "investments"} class:link="{selectedMenu === "investments"}" >
-      <Link   to="investments" >{$LanguageStore === "TR" ? "Yatırımlar" : "Investments"} </Link>
-     <div>
-      <Badge color="primary">{investmentsCount} {$LanguageStore === "TR" ? "İşlem" : "transactions"}</Badge>
-      <Badge color="primary">{$LanguageStore === "TR" ? "toplam" : "total"}: {totalInvestmentAmount ? totalInvestmentAmount : 0} </Badge>
-    </div>
-    </div>
-    
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={() => selectedMenu = "expenses"} class:link="{selectedMenu === "expenses"}" >
-      <Link  to="expenses" >{$LanguageStore === "TR" ? "Harcamalar" : "Expenses"} </Link>
-      <div>
-      <Badge color="primary">{expenseCount} {$LanguageStore === "TR" ? "İşlem" : "transactions"}</Badge>
-      <Badge color="primary">{$LanguageStore === "TR" ? "toplam" : "total"}: {totalExpenseAmount ? totalExpenseAmount : 0} </Badge>
-    </div>
-    </div>
-    
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={() => selectedMenu = "incomes"} class:link="{selectedMenu === "incomes"}" >
-      <Link   to="incomes" >{$LanguageStore === "TR" ? "Gelirler" : "Incomes"}</Link>
-      <div>
-        <Badge  color="primary">{incomesCount} {$LanguageStore === "TR" ? "İşlem" : "transactions"}</Badge>
-      <Badge color="primary">{$LanguageStore === "TR" ? "toplam" : "total"}: {totalIncomeAmount ? totalIncomeAmount : 0} </Badge>
+  <Router>
+    <div class="header">
+      <div
+        class="yeni"
+        on:click={() => (selectedMenu = "new")}
+        class:link={selectedMenu === "new"}
+      >
+        <Link to="form"
+          >{$LanguageStore === "TR" ? "Yeni İşlem Ekle" : "New Transaction"}
+          <span>+</span>
+        </Link>
       </div>
-      
+
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        on:click={() => (selectedMenu = "transitions")}
+        class:link={selectedMenu === "transitions"}
+      >
+        <Link to="/"
+          >{$LanguageStore === "TR" ? "Tüm İşlemler" : "All Transactions"}
+        </Link>
+      </div>
+
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        on:click={() => (selectedMenu = "investments")}
+        class:link={selectedMenu === "investments"}
+      >
+        <Link to="investments"
+          >{$LanguageStore === "TR" ? "Yatırımlar" : "Investments"}
+        </Link>
+        <div>
+          <Badge color="primary"
+            >{$countOfInvestment}
+            {$LanguageStore === "TR" ? "İşlem" : "transactions"}</Badge
+          >
+          <Badge color="primary"
+            >{$LanguageStore === "TR" ? "toplam" : "total"}: {$amountOfInvestment
+              ? $amountOfInvestment
+              : 0}
+          </Badge>
+        </div>
+      </div>
+
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        on:click={() => (selectedMenu = "expenses")}
+        class:link={selectedMenu === "expenses"}
+      >
+        <Link to="expenses"
+          >{$LanguageStore === "TR" ? "Harcamalar" : "Expenses"}
+        </Link>
+        <div>
+          <Badge color="primary"
+            >{$countOfExpense}
+            {$LanguageStore === "TR" ? "İşlem" : "transactions"}</Badge
+          >
+          <Badge color="primary"
+            >{$LanguageStore === "TR" ? "toplam" : "total"}: {$amountOfExpense
+              ? $amountOfExpense
+              : 0}
+          </Badge>
+        </div>
+      </div>
+
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        on:click={() => (selectedMenu = "incomes")}
+        class:link={selectedMenu === "incomes"}
+      >
+        <Link to="incomes"
+          >{$LanguageStore === "TR" ? "Gelirler" : "Incomes"}</Link
+        >
+        <div>
+          <Badge color="primary"
+            >{$countOfIncome}
+            {$LanguageStore === "TR" ? "İşlem" : "transactions"}</Badge
+          >
+          <Badge color="primary"
+            >{$LanguageStore === "TR" ? "toplam" : "total"}: {$amountOfIncome
+              ? $amountOfIncome
+              : 0}
+          </Badge>
+        </div>
+      </div>
+
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+
+      <div class="lang" on:click={handleLanguage}>
+        {$LanguageStore === "TR" ? "EN" : "TR"}
+      </div>
+
+      <div>
+        {$LanguageStore === "TR"
+          ? "Yatırımların Gelirlere Oranı"
+          : "Ratio of Investment to Incomes "}
+        <Progress
+          color="warning"
+          value={($amountOfInvestment / $amountOfIncome) * 100}
+        /> %{Math.round(
+          $amountOfIncome === 0
+            ? 0
+            : ($amountOfInvestment / $amountOfIncome) * 100
+        )}
+      </div>
+      <div>
+        {$LanguageStore === "TR"
+          ? "Giderlerin Gelirlere Oranı"
+          : "Ratio of Expenses to Incomes "}
+        <Progress
+          color="info"
+          value={($amountOfExpense / $amountOfIncome) * 100}
+        />% {Math.round(
+          $amountOfIncome === 0 ? 0 : ($amountOfExpense / $amountOfIncome) * 100
+        )}
+      </div>
     </div>
-    
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-   
-    <div class="lang" on:click={handleLanguage} >{$LanguageStore === "TR" ? "EN" : "TR"} </div>
-    
-    
-    <div>
-      {$LanguageStore === "TR" ? "Yatırımların Gelirlere Oranı" : "Ratio of Investment to Incomes "}
-          <Progress color="warning" value={(totalInvestmentAmount/totalIncomeAmount)*100} ></Progress> %{Math.round(totalIncomeAmount === 0 ? 0 : (totalInvestmentAmount/totalIncomeAmount)*100)}
 
-    </div>
-    <div  >
-      {$LanguageStore === "TR" ? "Giderlerin Gelirlere Oranı" : "Ratio of Expenses to Incomes "}
-          <Progress color="info" value={(totalExpenseAmount/totalIncomeAmount)*100} />% {Math.round(totalIncomeAmount === 0 ? 0 :(totalExpenseAmount/totalIncomeAmount)*100)} 
+    <main class="container">
+      <FeedbackStats />
+      <Route path="/">
+        <div class="list">
+          <FeedbackList />
+        </div>
+      </Route>
 
-    </div>
+      <Route path="form">
+        <FeedbackForm {selectedMenu} on:selectMenu={handleSelectMenu} />
+      </Route>
 
-
-   
-  </div>
-
-<main class="container" >
-  
-
-  
-  <FeedbackStats />
-  <Route path="/" >
-    <div class="list">
-    <FeedbackList />
-  </div>
-  </Route>
-
-  <Route path="form" >
-    <FeedbackForm {selectedMenu} on:selectMenu={handleSelectMenu} />
-  </Route>
-  
-  
-  <Route path="investments" >
-    <div class="list">
-    <Investments />
-  </div>
-  </Route>
-  <Route path="expenses" >
-    <div class="list">
-    <Expenses />
-  </div>
-  </Route>
-  <Route path="incomes" >
-    <div class="list">
-    <Incomes />
-  </div>
-  </Route>
-  
-  
-  
-  
-</main> 
-</Router>
- 
-
+      <Route path="investments">
+        <div class="list">
+          <Investments />
+        </div>
+      </Route>
+      <Route path="expenses">
+        <div class="list">
+          <Expenses />
+        </div>
+      </Route>
+      <Route path="incomes">
+        <div class="list">
+          <Incomes />
+        </div>
+      </Route>
+    </main>
+  </Router>
 </div>
 
 <style>
-  .yeni{
+  .yeni {
     font-size: 23px;
   }
-  span{
+  span {
     width: 30px;
     height: 20px;
     background-color: red;
@@ -166,4 +207,3 @@
     padding: 10px;
   }
 </style>
-
--- src/stores.js
+++ src/stores.js
@@ -1,9 +1,40 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 import { persistStore } from "./persistStore";
 
 const initial = []
 
 export const FeedbackStore = persistStore('data', initial)
+
+export const countOfIncome = derived(
+    FeedbackStore,
+    // @ts-ignore
+    $FeedbackStore => $FeedbackStore.filter((item) => item.transitionType === "Income").length
+)
+export const amountOfIncome = derived(
+    FeedbackStore,
+    // @ts-ignore
+    $FeedbackStore => $FeedbackStore.filter((item) => item.transitionType === "Income").reduce((a, item) => a + item.amount, 0)
+)
+export const countOfExpense = derived(
+    FeedbackStore,
+    // @ts-ignore
+    $FeedbackStore => $FeedbackStore.filter((item) => item.transitionType === "Expense").length
+)
+export const amountOfExpense = derived(
+    FeedbackStore,
+    // @ts-ignore
+    $FeedbackStore => $FeedbackStore.filter((item) => item.transitionType === "Expense").reduce((a, item) => a + item.amount, 0)
+)
+export const countOfInvestment = derived(
+    FeedbackStore,
+    // @ts-ignore
+    $FeedbackStore => $FeedbackStore.filter((item) => item.transitionType === "Investment").length
+)
+export const amountOfInvestment = derived(
+    FeedbackStore,
+    // @ts-ignore
+    $FeedbackStore => $FeedbackStore.filter((item) => item.transitionType === "Investment").reduce((a, item) => a + item.amount, 0)
+)
 
 
 const initialLanguage = "EN"
