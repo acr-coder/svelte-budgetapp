530f493e0c5447d3c7725189923ba3d08ba95623
devweb550 <devweb550@gmail.com>
2022-11-02 15:48:33 +0300

    search update

--- src/lib/FeedbackList.svelte
+++ src/lib/FeedbackList.svelte
@@ -1,23 +1,43 @@
 <script>
   
-  import { FeedbackStore } from '../stores';
+  import { FeedbackStore, LanguageStore } from '../stores';
   import { fly, fade, slide, scale } from 'svelte/transition';
   import { flip } from 'svelte/animate'
   import FeedbackItem from "./FeedbackItem.svelte";
-
+  import { FormGroup,Label, Input} from 'sveltestrap';
   
-
+  let search = undefined;
+	$: transactions = $FeedbackStore;
+	$: visibleTransactions = search ?
+		transactions.filter(transaction => {
+			return transaction.text.match(`${search.toLocaleLowerCase()}.*`) || transaction.date.match(`${search.toLocaleLowerCase()}.*`)
+		}) : transactions;
 
 
 </script>
 
-{#each $FeedbackStore as fb (fb.id) }
+  
+  <!-- <Input type="search"  bind:value={search}  placeholder="Search" /> -->
+  <FormGroup>
+    <Label>{$LanguageStore === "TR" ? "İşlem Ara" : "Search For Transactions"}</Label>
+    <Input type="text" bind:value={search}  />
+  </FormGroup>
+
+  {#each visibleTransactions as fb (fb.id) }
+
 <div class="itemList"  in:fade out:scale|local={{duration:1000}} animate:flip={{duration: 1000}}  >
   
   <FeedbackItem {fb} />
 </div>
     
 {/each}
+
+
+
+
+
+  
+
 
 <style>
   @media screen and (min-width: 600px) {
