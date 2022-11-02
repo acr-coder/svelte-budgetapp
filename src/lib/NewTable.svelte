c89f77e4a26b480f7822f8ebd0b15900452819f6
devweb550 <devweb550@gmail.com>
2022-11-02 13:55:39 +0300

    table update

--- src/lib/NewTable.svelte
+++ src/lib/NewTable.svelte
@@ -24,14 +24,14 @@ import { Card, CardBody, CardHeader, Input, Table, Column, Styles, Button } from
 		<Input type="search" bind:value={search} class="ms-auto w-auto" placeholder="Search" />
 	</CardHeader>
 	<CardBody>
-		<Table rows={visibleTransactions} let:row={transaction} hover responsive >
-			<Column header={$LanguageStore === "TR" ? "İşlem Adı" : "Transaction Name"}>
+		<Table  rows={visibleTransactions} let:row={transaction} responsive  hover class="text-xs sm:text-lg" >
+			<Column header={$LanguageStore === "TR" ? "İşlem" : "Transaction"}  >
 				{transaction.text}
 			</Column>
-			<Column header={$LanguageStore === "TR" ? "İşlem Miktarı" : "Amount"}>
+			<Column header={$LanguageStore === "TR" ? "Miktar" : "Amount"}>
 				{transaction.amount}
 			</Column>
-			<Column header={$LanguageStore === "TR" ? "İşlem Çeşidi" : "Transaction Type"}>
+			<Column header={$LanguageStore === "TR" ? "Çeşidi" : "Type"}>
 				{transaction.transitionType}
 			</Column>
 			<Column header={$LanguageStore === "TR" ? "Tarih" : "Date"}>
